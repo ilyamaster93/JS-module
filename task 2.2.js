6ed96269dc8f209d2d85f65ef20a5a39db1709a4
shchukin <ilyamaster93@gmail.com>
2020-07-20 20:51:12 +0300

    c5

---
+++ task 2.2.js
@@ -0,0 +1,7 @@
+function lenghtCounter () {
+    var array = ["Газета", "Журнал", "Книга", "Учебник", "Листовка"]
+    sum=+sum;
+    console.log ("Длина массива " + sum);
+    }
+array.forEach(lenghtCounter);
+    
